59c2448301806f4c207fd441b6eb8e6d81ce31bf
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-04-22 23:11:19 +0300

    Add lb_Login

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lb_Login = new Label();
+            SuspendLayout();
+            // 
+            // lb_Login
+            // 
+            lb_Login.AutoSize = true;
+            lb_Login.Location = new Point(18, 12);
+            lb_Login.Margin = new Padding(5, 0, 5, 0);
+            lb_Login.Name = "lb_Login";
+            lb_Login.Size = new Size(64, 28);
+            lb_Login.TabIndex = 0;
+            lb_Login.Text = "Login";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Brown;
+            ClientSize = new Size(273, 245);
+            Controls.Add(lb_Login);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(5, 4, 5, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lb_Login;
     }
 }
