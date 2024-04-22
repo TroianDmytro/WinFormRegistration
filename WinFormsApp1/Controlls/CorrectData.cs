c530b69795b41c8ef27874f8ccd827eded947229
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-04-22 23:56:08 +0300

    Modifier CorrectData
    Changing access modirier to public static, Add method isStronPasswoed and method IsPasswordValid

--- WinFormsApp1/Controlls/CorrectData.cs
+++ WinFormsApp1/Controlls/CorrectData.cs
@@ -7,8 +7,24 @@ using System.Threading.Tasks;
 
 namespace WinFormsApp1.Controlls
 {
-    internal class CorrectData
+    public static class CorrectData
     {
-      
+        public static bool IsStrongPassword(string password)
+        {
+            if (IsPasswordValid(password))
+            {
+                // Перевірка чи пароль містить хоча б одну цифру, одну велику літеру та один спеціальний символ
+                return Regex.IsMatch(password, @"^(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]+$");
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static bool IsPasswordValid(string password)
+        {
+            // Перевірка довжини пароля
+            return password.Length >= 6 && password.Length <= 12;
+        }
     }
 }
